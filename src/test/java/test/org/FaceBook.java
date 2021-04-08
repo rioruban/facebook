180550987b9a31a5a055711164ad7c185f947599
rioruban <rubanrajrio@gamil.com>
2021-04-08 18:30:23 +0530

    task completed

---
+++ src/test/java/test/org/FaceBook.java
@@ -0,0 +1,24 @@
+package test.org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FaceBook {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ELCOT\\eclipse-workspace\\Selenium\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.id("email"));
+		username.sendKeys("rio");
+
+		WebElement userpassword = driver.findElement(By.id("pass"));
+		userpassword.sendKeys("pass");
+		 driver.findElement(By.name("login")).click();
+
+	}
+
+}
